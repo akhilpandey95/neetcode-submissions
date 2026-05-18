8e1555f6122968b5946577497faca962f44a2beb
Akhil Pandey <akhilpandey095@gmail.com>
2026-05-18 09:36:46 -0500

    Add: two-integer-sum - submission-4

---
+++ Data Structures & Algorithms/two-integer-sum/submission-4.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        if len(nums) >= 2 and len(nums) <= 1000:
+            for num_i in range(0, len(nums) - 1):
+                for num_j in range(1, len(nums)):
+                    if nums[num_i] + nums[num_j] == target and num_i != num_j:
+                        return [num_i, num_j]
+
